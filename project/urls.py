f936f70eb161703c5fc3a04ddb86d2e733aeedd2
Abdullah Mazen Al-Haddad <57428346+Leddark@users.noreply.github.com>
2021-08-08 15:17:41 +0300

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path , include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
     path('', include('Home.url')),
     path('port_scan/', include('port_scan.url')),
     path('sub_domin/', include('sub_domin.url')),
